697412305ec0dd444fa69a41b216f17992eccb69
Lakshy Gupta <83910804+lakshy8734@users.noreply.github.com>
2024-02-28 11:20:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const app = express();
+const port = 5000;
+const mongoDb = require("./db");
+mongoDb();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+})
+
+app.use('/api', require('./Routes/CreateUser'));
+app.use('/api', require('./Routes/Displaydata'));
+app.use('/api', require('./Routes/OrderData'));
+
+app.listen(port, () => {
+    console.log(`app listening on ${port}`);
+})
